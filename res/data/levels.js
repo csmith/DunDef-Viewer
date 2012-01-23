0c68cdf27d1627793bb514195d1e7c863541b584
Chris Smith <chris@dmdirc.com>
2012-01-23 23:54:16 +0000

    Offsets and scaling for some more maps

--- res/data/levels.js
+++ res/data/levels.js
@@ -1,16 +1,16 @@
 var levels = [
 
- {name: 'The Deeper Well', minimap: 'res/images/minimaps/Level1.png', image: 'res/images/levels/Level1.jpg'},
+ {name: 'The Deeper Well', minimap: 'res/images/minimaps/Level1.png', image: 'res/images/levels/Level1.jpg', offsets: {left: 130, top: 65}, scale: {left: 1.19, top: 1.17}},
  {name: 'Foundries and Forges', minimap: 'res/images/minimaps/Level2.png', image: 'res/images/levels/Level2.jpg'},
  {name: 'Magus Quarters', minimap: 'res/images/minimaps/Level3.png', image: 'res/images/levels/Level3.jpg'},
- {name: 'Alchemical Laboratory', minimap: 'res/images/minimaps/Level4.png', image: 'res/images/levels/Level4.jpg'},
+ {name: 'Alchemical Laboratory', minimap: 'res/images/minimaps/Level4.png', image: 'res/images/levels/Level4.jpg', offsets: {left: 280, top: 110}, scale: {left: 0.92, top: 0.92}},
  {name: 'Servants Quarters', minimap: 'res/images/minimaps/Level5.png', image: 'res/images/levels/Level5.jpg'},
  {name: 'Castle Armory', minimap: 'res/images/minimaps/Level6.png', image: 'res/images/levels/Level6.jpg'},
  {name: 'Hall of Court', minimap: 'res/images/minimaps/Level7.png', image: 'res/images/levels/Level7.jpg'},
  {name: 'The Throne Room', minimap: 'res/images/minimaps/Level8.png', image: 'res/images/levels/Level8.jpg'},
  {name: 'Royal Gardens', minimap: 'res/images/minimaps/RoyalGardens.png', image: 'res/images/levels/Level9.jpg'},
- {name: 'The Ramparts', minimap: 'res/images/minimaps/Level9.png', image: 'res/images/levels/Level10.jpg'},
+ {name: 'The Ramparts', minimap: 'res/images/minimaps/Level9.png', image: 'res/images/levels/Level10.jpg', offsets: {left: 142, top: 0}, scale: {left: 1.08, top: 1.1}},
  {name: 'Endless Spires', minimap: 'res/images/minimaps/TheSpires.png', image: 'res/images/levels/Level11.jpg', offsets: {left: 142, top: 57}, scale: {left: 1.04, top: 1.04}},
- {name: 'The Summit', minimap: 'res/images/minimaps/TheSummit.png', image: 'res/images/levels/Level12.jpg'},
+ {name: 'The Summit', minimap: 'res/images/minimaps/TheSummit.png', image: 'res/images/levels/Level12.jpg', offsets: {left: 200, top: 115}},
 
 ];
