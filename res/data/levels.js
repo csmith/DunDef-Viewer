5ef60e96d5b8501275a9d2440589ceef250992dd
Chris Smith <chris@dmdirc.com>
2012-01-23 15:20:44 +0000

    Missing/fixed assets
    Add data about towers and levels
    Show one of each tower on the page

---
+++ res/data/levels.js
@@ -0,0 +1,16 @@
+var levels = [
+
+ {name: 'The Deeper Well', minimap: 'res/images/minimaps/Level1.png', image: 'res/images/levels/Level1.jpg'},
+ {name: 'Foundries and Forges', minimap: 'res/images/minimaps/Level2.png', image: 'res/images/levels/Level2.jpg'},
+ {name: 'Magus Quarters', minimap: 'res/images/minimaps/Level3.png', image: 'res/images/levels/Level3.jpg'},
+ {name: 'Alchemical Laboratory', minimap: 'res/images/minimaps/Level4.png', image: 'res/images/levels/Level4.jpg'},
+ {name: 'Servants Quarters', minimap: 'res/images/minimaps/Level5.png', image: 'res/images/levels/Level5.jpg'},
+ {name: 'Castle Armory', minimap: 'res/images/minimaps/Level6.png', image: 'res/images/levels/Level6.jpg'},
+ {name: 'Hall of Court', minimap: 'res/images/minimaps/Level7.png', image: 'res/images/levels/Level7.jpg'},
+ {name: 'The Throne Room', minimap: 'res/images/minimaps/Level8.png', image: 'res/images/levels/Level8.jpg'},
+ {name: 'Royal Gardens', minimap: 'res/images/minimaps/RoyalGardens.png', image: 'res/images/levels/Level9.jpg'},
+ {name: 'The Ramparts', minimap: 'res/images/minimaps/Level9.png', image: 'res/images/levels/Level10.jpg'},
+ {name: 'Endless Spires', minimap: 'res/images/minimaps/TheSpires.png', image: 'res/images/levels/Level11.jpg'},
+ {name: 'The Summit', minimap: 'res/images/minimaps/TheSummit.png', image: 'res/images/levels/Level12.jpg'},
+
+];
