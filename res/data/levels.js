6244ee42701e71d0401cd8333afe82be0865f19b
Chris Smith <chris@dmdirc.com>
2012-01-24 00:49:53 +0000

    Default max DU for each map

--- res/data/levels.js
+++ res/data/levels.js
@@ -1,16 +1,96 @@
 var levels = [
 
- {name: 'The Deeper Well', minimap: 'res/images/minimaps/Level1.png', image: 'res/images/levels/Level1.jpg', offsets: {left: 130, top: 65}, scale: {left: 1.19, top: 1.17}},
- {name: 'Foundries and Forges', minimap: 'res/images/minimaps/Level2.png', image: 'res/images/levels/Level2.jpg'},
- {name: 'Magus Quarters', minimap: 'res/images/minimaps/Level3.png', image: 'res/images/levels/Level3.jpg'},
- {name: 'Alchemical Laboratory', minimap: 'res/images/minimaps/Level4.png', image: 'res/images/levels/Level4.jpg', offsets: {left: 280, top: 110}, scale: {left: 0.92, top: 0.92}},
- {name: 'Servants Quarters', minimap: 'res/images/minimaps/Level5.png', image: 'res/images/levels/Level5.jpg'},
- {name: 'Castle Armory', minimap: 'res/images/minimaps/Level6.png', image: 'res/images/levels/Level6.jpg'},
- {name: 'Hall of Court', minimap: 'res/images/minimaps/Level7.png', image: 'res/images/levels/Level7.jpg'},
- {name: 'The Throne Room', minimap: 'res/images/minimaps/Level8.png', image: 'res/images/levels/Level8.jpg'},
- {name: 'Royal Gardens', minimap: 'res/images/minimaps/RoyalGardens.png', image: 'res/images/levels/Level9.jpg'},
- {name: 'The Ramparts', minimap: 'res/images/minimaps/Level9.png', image: 'res/images/levels/Level10.jpg', offsets: {left: 142, top: 0}, scale: {left: 1.08, top: 1.1}},
- {name: 'Endless Spires', minimap: 'res/images/minimaps/TheSpires.png', image: 'res/images/levels/Level11.jpg', offsets: {left: 142, top: 57}, scale: {left: 1.04, top: 1.04}},
- {name: 'The Summit', minimap: 'res/images/minimaps/TheSummit.png', image: 'res/images/levels/Level12.jpg', offsets: {left: 200, top: 115}},
+ {
+  name: 'The Deeper Well',
+  minimap: 'res/images/minimaps/Level1.png',
+  image: 'res/images/levels/Level1.jpg',
+  du: 60,
+  offsets: {left: 130, top: 65},
+  scale: {left: 1.19, top: 1.17}
+ },
+
+ {
+  name: 'Foundries and Forges',
+  minimap: 'res/images/minimaps/Level2.png',
+  image: 'res/images/levels/Level2.jpg',
+  du: 80
+ },
+
+ {
+  name: 'Magus Quarters',
+  minimap: 'res/images/minimaps/Level3.png',
+  image: 'res/images/levels/Level3.jpg',
+  du: 90
+ },
+
+ {
+  name: 'Alchemical Laboratory',
+  minimap: 'res/images/minimaps/Level4.png',
+  image: 'res/images/levels/Level4.jpg',
+  du: 85,
+  offsets: {left: 280, top: 110},
+  scale: {left: 0.92, top: 0.92}
+ },
+
+ {
+  name: 'Servants Quarters',
+  minimap: 'res/images/minimaps/Level5.png',
+  image: 'res/images/levels/Level5.jpg',
+  du: 85
+ },
+
+ {
+  name: 'Castle Armory',
+  minimap: 'res/images/minimaps/Level6.png',
+  image: 'res/images/levels/Level6.jpg',
+  du: 90
+ },
+
+ {
+  name: 'Hall of Court',
+  minimap: 'res/images/minimaps/Level7.png',
+  image: 'res/images/levels/Level7.jpg',
+  du: 100
+ },
+
+ {
+  name: 'The Throne Room',
+  minimap: 'res/images/minimaps/Level8.png',
+  image: 'res/images/levels/Level8.jpg',
+  du: 100
+ },
+
+ {
+  name: 'Royal Gardens',
+  minimap: 'res/images/minimaps/RoyalGardens.png',
+  image: 'res/images/levels/Level9.jpg',
+  du: 130
+ },
+
+ {
+  name: 'The Ramparts',
+  minimap: 'res/images/minimaps/Level9.png',
+  image: 'res/images/levels/Level10.jpg',
+  du: 110,
+  offsets: {left: 142, top: 0},
+  scale: {left: 1.08, top: 1.1}
+ },
+
+ {
+  name: 'Endless Spires',
+  minimap: 'res/images/minimaps/TheSpires.png',
+  image: 'res/images/levels/Level11.jpg',
+  du: 110,
+  offsets: {left: 142, top: 57},
+  scale: {left: 1.04, top: 1.04}
+ },
+
+ {
+  name: 'The Summit',
+  minimap: 'res/images/minimaps/TheSummit.png',
+  image: 'res/images/levels/Level12.jpg',
+  du: 150,
+  offsets: {left: 200, top: 115}
+ },
 
 ];
